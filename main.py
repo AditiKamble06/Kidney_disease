4bc5b736554e20021f72fff7503921d3f41c5e72
Aditi06 <aditi.kamble.che22@itbhu.ac.in>
2024-07-21 11:14:35 +0530

    prepare base model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from src.kidney_Disease import logger
 from src.kidney_Disease.pipeline.stage1_dataingestion import DataIngestionTrainingPipeline
-
+from src.kidney_Disease.pipeline.stage2_prep_base_model import PrepareBaseModelTrainingPipeline
 
 
 
@@ -9,6 +9,16 @@ try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+STAGE_NAME = "Prepare base model"
+try: 
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
